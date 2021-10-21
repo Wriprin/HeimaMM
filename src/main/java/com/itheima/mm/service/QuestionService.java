b031ee61b625a58b0acd393e21c476232d15ca13
Wriprin <wriprin@gmail.com>
2021-10-21 19:27:17 +0800

    add transaction of addBasicQuestionContent func

--- src/main/java/com/itheima/mm/service/QuestionService.java
+++ src/main/java/com/itheima/mm/service/QuestionService.java
@@ -10,6 +10,7 @@ import com.itheima.mm.pojo.Tag;
 import com.itheima.mm.utils.SqlSessionFactoryUtils;
 import org.apache.ibatis.session.SqlSession;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -48,40 +49,49 @@ public class QuestionService {
      * @param question
      */
     public void addBasicQuestion(Question question) throws Exception {
-        SqlSession sqlSession = SqlSessionFactoryUtils.openSqlSession();
-        QuestionDao questionDao = sqlSession.getMapper(QuestionDao.class);
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = SqlSessionFactoryUtils.openSqlSession();
+            QuestionDao questionDao = sqlSession.getMapper(QuestionDao.class);
 
-        // 1. 调用 questionDao 层，添加 t_question 数据
-        questionDao.addBasicQuestion(question);
+            // 1. 调用 questionDao 层，添加 t_question 数据
+            questionDao.addBasicQuestion(question);
 
-        // 2. 判断 questionItemList 是否为空，如果不为空，则将所有选项添加到 tr_question_item
-        // 调用 questionItemDao 层，添加 tr_question_item 数据
-        QuestionItemDao questionItemDao = sqlSession.getMapper(QuestionItemDao.class);
-        List<QuestionItem> questionItemList = question.getQuestionItemList();
-        if (questionItemList != null && questionItemList.size() > 0) {
-            // 不为空，添加数据
-            for (QuestionItem questionItem : questionItemList) {
-                // 手动设置 questionId
-                questionItem.setQuestionId(question.getId());
-                // 调用 dao，添加 数据
-                questionItemDao.add(questionItem);
+            // 2. 判断 questionItemList 是否为空，如果不为空，则将所有选项添加到 tr_question_item
+            // 调用 questionItemDao 层，添加 tr_question_item 数据
+            QuestionItemDao questionItemDao = sqlSession.getMapper(QuestionItemDao.class);
+            List<QuestionItem> questionItemList = question.getQuestionItemList();
+            if (questionItemList != null && questionItemList.size() > 0) {
+                // 不为空，添加数据
+                for (QuestionItem questionItem : questionItemList) {
+                    // 手动设置 questionId
+                    questionItem.setQuestionId(question.getId());
+                    // 调用 dao，添加 数据
+                    questionItemDao.add(questionItem);
+                }
             }
-        }
 
-        // 3. 存储 题目和标签关联信息 到 tr_question_tag
-        // 判断是否为 null
-        List<Tag> tagList = question.getTagList();
-        if (tagList != null && tagList.size() > 0) {
-            // 不为空，添加数据
-            for (Tag tag : tagList) {
-                // 存储到 map 中
-                Map tagMap = new HashMap<>();
-                tagMap.put("questionId", question.getId());
-                tagMap.put("tagId", tag.getId());
-                questionDao.addQuestionTag(tagMap);
+            // 3. 存储 题目和标签关联信息 到 tr_question_tag
+            // 判断是否为 null
+            List<Tag> tagList = question.getTagList();
+            if (tagList != null && tagList.size() > 0) {
+                // 不为空，添加数据
+                for (Tag tag : tagList) {
+                    // 存储到 map 中
+                    Map tagMap = new HashMap<>();
+                    tagMap.put("questionId", question.getId());
+                    tagMap.put("tagId", tag.getId());
+                    questionDao.addQuestionTag(tagMap);
+                }
             }
-        }
 
-        SqlSessionFactoryUtils.commitAndClose(sqlSession);
+            // 提交事务
+            SqlSessionFactoryUtils.commitAndClose(sqlSession);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            // 回滚事务
+            SqlSessionFactoryUtils.rollbackAndClose(sqlSession);
+        }
     }
 }
